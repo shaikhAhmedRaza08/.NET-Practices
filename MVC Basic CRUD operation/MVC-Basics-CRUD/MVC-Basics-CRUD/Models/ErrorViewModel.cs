35373355dff624b9516b6472a31c994653d95467
shaikhAhmedRaza08 <shaikhahmedraza72@outlook.com>
2022-09-11 11:42:03 +0530

    first commit

---
+++ MVC Basic CRUD operation/MVC-Basics-CRUD/MVC-Basics-CRUD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC_Basics_CRUD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
